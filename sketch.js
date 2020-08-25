836059b0ea4a1f657921db8f31f3e2e96fc9f47b
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2020-08-25 22:27:16 +0530

    Update sketch.js
    RightEdge and leftEdge declaration

--- sketch.js
+++ sketch.js
@@ -1,40 +1,57 @@
-
 var bullet, wall;
 var speed, thickness, weight;
-var gameState = "play";
+var leftEdge, rightEdge;
 
 function setup() {
   createCanvas(1600,400);
 
 speed = random(223, 321);
-thickness = random(400, 1500);
+thickness = random(30,90);
 weight = random(30, 52);
   
   bullet = createSprite(80, 200, 20, 20);
   bullet.shapeColor = color(255, 255, 255);
-
-
+  bullet.velocityX = speed/10;
   wall = createSprite(1500, 200, 10, 200);
-  wall.shapeColor = color(255, 255, 255);
+  wall.shapeColor = color(230,230,230);
+
+  
 
 }
 
 function draw() {
   background(0,0,0);  
   
-  bullet.velocityX = speed/10;
+ 
 
-  // telling the car when to stop
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX = 0;
+    var damage = (weight*speed*speed)/(2*thickness*thickness*thickness);
 
-  if (wall.x - bullet.x < wall.width/2 + bullet.width/2 && weight*speed*speed/thickness*thickness*thickness*2 < 10){
-   bullet.velocityX = 0;
-   wall.shapeColor = color(0, 0, 255);
+    if(damage > 10){
+      wall.shapeColor = color(255, 0, 0);
+    }
+
+    if(damage < 10){
+      wall.shapeColor = color(0, 255, 0);
+    }
   }
 
-  if (wall.x - bullet.x < wall.width/2 + bullet.width/2 && weight*speed*speed/thickness*thickness*thickness*2 > 10){
-    bullet.velocityX = 0;
-    wall.shapeColor = color(0, 255, 0);
-   }
-
   drawSprites();
+}
+
+function hasCollided(bullet,wall){
+
+    rightEdge = bullet.x + bullet.width/2;
+  leftEdge = wall.x - wall.width/2;
+  //leftEdge = wall.x ;
+
+    if (rightEdge>=leftEdge)
+	{
+		return true;
+	}
+	return false;
+
+ 
+
 }
